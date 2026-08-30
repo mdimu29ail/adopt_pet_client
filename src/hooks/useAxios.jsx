3f52481aabc56ba6ed7f1ea53f08d12aa0ecddea
mdimu29ail <mdimu29@gmail.com>
2026-08-31 03:01:32 +0600

    add link

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -4,7 +4,7 @@ const axiosPublic = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
     // 'http://localhost:5000' ||
-    'http://localhost:5000',
+    'https://pet-adopton-sarver.vercel.app',
 });
 
 const useAxios = () => {
